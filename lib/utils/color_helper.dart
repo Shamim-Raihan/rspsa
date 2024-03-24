675f57eb0cea704c8d292dd1e6fbd000024d0dbe
Shamim Raihan <shamimraihan91@gmail.com>
2024-03-24 21:10:55 +0600

    initial

---
+++ lib/utils/color_helper.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorHelper {
+  static const Color primeryColor = Color.fromARGB(255, 46, 132, 213);
+  static const Color secondaryColor = Colors.red;
+}
